a8954e84df0c83d2f7ce7063ac69443eed4cf607
LTurret <a0919329@gmail.com>
2023-07-04 14:30:16 +0800

    content update

--- 4. DOMTree/app.js
+++ 4. DOMTree/app.js
@@ -1,6 +1,11 @@
-let button = document.querySelector("button");
+let a = document.querySelector(".a");
+let b = document.querySelector(".b");
 
-button.addEventListener("click", () => {
-  let form = document.querySelector("form");
-  form.reset();
+a.addEventListener("click", () => {
+  alert("You have pressed red");
+});
+
+b.addEventListener("click", (e) => {
+  e.stopPropagation();
+  alert("You have pressed blue");
 });
